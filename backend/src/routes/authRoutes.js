82dc931d5f0b291a57ba86abb4a3c62226abeede
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-01-19 20:15:40 +0700

    feat: bổ sung api endpoints cho chức năng quên mật khẩu

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser, loginGoogle } = require('../controllers/authController');
+const { registerUser, loginUser, loginGoogle, forgotPassword, resetPassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/google', loginGoogle);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
