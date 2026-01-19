5da7f83cf6b634e2f65346c98b3e33ce7a72d9ff
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-01-19 15:41:41 +0700

    feat: thêm api endpoint cho google login

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, loginGoogle } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/google', loginGoogle);
 
 module.exports = router;
