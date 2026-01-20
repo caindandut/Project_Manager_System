8fa5981196b70916cea8fb72447a3efaa65e2793
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-01-20 21:58:18 +0700

    feat: thêm api endpoint lấy thông tin user và áp dụng middlewares

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { registerUser, loginUser, loginGoogle, forgotPassword, resetPassword } = require('../controllers/authController');
+const { registerUser, loginUser, loginGoogle, forgotPassword, resetPassword, getMe } = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
@@ -7,5 +8,6 @@ router.post('/login', loginUser);
 router.post('/google', loginGoogle);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
+router.get('/me', protect, getMe);
 
 module.exports = router;
