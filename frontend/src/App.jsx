ebba8a692fa414b187cc7e1b98c8b492af67d917
ledangkhanhdut <khanhdangabc2@gmail.com>
2026-01-21 22:44:26 +0700

    build: khởi tạo dự án FE bằng VITE và cấu hình TWCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">
+        Hello Tailwind v3!
+      </h1>
+    </div>
+  )
+}
+
+export default App
